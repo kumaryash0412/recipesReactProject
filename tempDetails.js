6e961e1e69f25b429402e5cdbc35541bc5b76b2b
bairayash <yashwanthb1994@gmail.com>
2020-03-22 12:24:43 -0400

    recipes project

---
+++ tempDetails.js
@@ -0,0 +1,16 @@
+export const recipe = {
+    f2f_url: "https://www.food2fork.com/view/Jalapeno_Popper_Grilled_Cheese_Sandwich/35382",
+    image_url:"https://www.closetcooking.com/wp-content/uploads/2013/04/JalapenoPopperGrilledCheeseSandwich1500.jpg",
+    ingredients: [
+        "2 jalapeno peppers, cut in half lengthwise and seeded",
+        "2 slices sour dough bread",
+        "1 tablespoon butter, room temperature",
+        "2 tablespoons cream cheese, room temperature",
+        "1/2 cup jack and cheddar cheese, shredded",
+        "1 tablespoon tortilla chips, crumbled"
+    ],
+    publisher: "Closet Cooking",
+    publisher_url: "http://closetcooking.com",
+    sourece_url: "https://www.closetcooking.com/jalapeno-popper-grilled-cheese-sandwich.html",
+    title: "Jalapeno Popper Grilled Cheese Sandwich"
+};
---
+++ tempList.js
@@ -0,0 +1,27 @@
+export const recipes = [
+    {
+        recipe_id:35382,
+        image_url:"https://www.closetcooking.com/wp-content/uploads/2013/04/JalapenoPopperGrilledCheeseSandwich1500.jpg",
+        title: "Jalapeno Popper Grilled Cheese Sandwich",
+        publisher:"Closet Cooking",
+        sourece_url: "https://www.closetcooking.com/jalapeno-popper-grilled-cheese-sandwich.html"
+    },
+
+    {
+        recipe_id:35383,
+        image_url:"https://www.closetcooking.com/wp-content/uploads/2013/04/JalapenoPopperGrilledCheeseSandwich1500.jpg",
+        title: "Jalapeno Popper Grilled Cheese Sandwich",
+        publisher:"Closet Cooking",
+        sourece_url: "https://www.closetcooking.com/jalapeno-popper-grilled-cheese-sandwich.html"
+    },
+
+    {
+        recipe_id:35384,
+        image_url:"https://www.closetcooking.com/wp-content/uploads/2013/04/JalapenoPopperGrilledCheeseSandwich1500.jpg",
+        title: "Jalapeno Popper Grilled Cheese Sandwich",
+        publisher:"Closet Cooking",
+        sourece_url: "https://www.closetcooking.com/jalapeno-popper-grilled-cheese-sandwich.html"
+    },
+
+
+]
